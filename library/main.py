4b46cdb427d2d92bd52d99c19c061cf66b7d0683
Frederico <fredericobsantos@gmail.com>
2021-04-24 18:14:37 +0100

    start working on new library

---
+++ library/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Population:
+    def __init__(self) -> None:
+        pass
+    def selection(self) -> None:
+        pass
+    def crossover(self) -> None:
+        pass
+    def mutation(self) -> None:
+        pass
+    def evolve(self) -> None:
+        pass

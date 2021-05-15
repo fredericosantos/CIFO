8625b4d72074aa52580f1b3b7f5359bc5e2ad03d
Frederico <fredericobsantos@gmail.com>
2021-05-15 01:42:48 +0100

    updating GAO

--- library/genetic_algorithm/crossover.py
+++ library/genetic_algorithm/crossover.py
@@ -1,11 +1,14 @@
 import random, copy
+from ssl import OP_NO_TLSv1_1
 
-def single_point_xo(p1: list, p2: list):
+
+def single_point_co(p1: list, p2: list):
     poopoo = random.randint(1, len(p1))
     offspring1 = p1[:poopoo] + p2[:poopoo]
     offspring2 = p1[poopoo:] + p2[poopoo:]
 
     return offspring1, offspring2
+
 
 def cycle_co(p1: list, p2: list):
     r_len = len(p1)
@@ -23,3 +26,40 @@ def cycle_co(p1: list, p2: list):
     o1 = [offspring1[i] for i in range(r_len)]
     o2 = [offspring2[i] for i in range(r_len)]
     return o1, o2
+
+
+def arithmetic_co(p1: list, p2: list):
+    # set alpha
+    a = random.uniform(0, 1)
+    o1 = [p1[i] * a + (1 - a) * p2[i] for i, _ in enumerate(p1)]
+    o2 = [p2[i] * a + (1 - a) * p1[i] for i, _ in enumerate(p1)]
+    return o1, o2
+
+
+def pmx_co(p1: list, p2: list):
+    idx1, idx2 = sorted(random.sample(range(len(p1)), 2))
+    o1, o2 = p1, p2
+    p1_substring = p1[idx1:idx2]
+    p2_substring = p2[idx1:idx2]
+    o1_dict = {}
+    o2_dict = {}
+
+    for i, n in enumerate(p2_substring):
+        o1_dict[o1.index(n)] = p1_substring[i]
+    for i, n in enumerate(p1_substring):
+        o2_dict[o2.index(n)] = p2_substring[i]
+    o1[idx1:idx2] = p2_substring
+    o2[idx1:idx2] = p1_substring
+
+    for i, v in o1_dict.items():
+        o1[i] = v
+    for i, v in o2_dict.items():
+        o2[i] = v
+    return o1, o2
+
+
+if __name__ == "__main__":
+    p1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    p2 = [9, 3, 7, 8, 2, 6, 5, 1, 4]
+
+    print(pmx_co(p1, p2))
--- library/genetic_algorithm/gao.py
+++ library/genetic_algorithm/gao.py
@@ -3,34 +3,35 @@ from main import Population, Individual
 import numpy as np
 
 
-def GAO(
-    pop: Population, generations: int, select, crossover, mutation
-) -> Individual:
-    for i in pop.individuals:
-        pop.fitness(i)
-    # Check if we want elitism and how many elites we keep
-    if (pop.n_elites > 0):
-        pop_prime = sorted(pop.individuals, key= lambda i: i.fitness)[:pop.n_elites]
-    else:
-        pop_prime = []
-    new_pop = []
-    # Evolve the rest of the population
-    while len(pop_prime) < len(pop):
-        # Select parents from population the representations of the parents
-        i1, i2 = pop.select(), pop.select()
-
-        # Apply crossover ~ probability is given to function
-        i1, i2 = pop.crossover(i1, i2)
-
-        for i in [i1, i2]:
-            # Apply mutation ~ probability is given to function
-            i = pop.mutation(i)
-            # Create individual and get its fitness
-            i = Individual(representation=i)
+def GAO(pop: Population, generations: int) -> Individual:
+    for _ in range(generations):
+        for i in pop.individuals:
             pop.fitness(i)
-        
-        new_pop.append(i1)
-        if len(new_pop) < len(pop):
-            new_pop.append(i2)
-    
-    pop.individuals = new_pop
+        # Check if we want elitism and how many elites we keep
+        if pop.n_elites > 0:
+            pop_prime = sorted(pop.individuals, key=lambda i: i.fitness)[: pop.n_elites]
+        else:
+            pop_prime = []
+        new_pop = []
+        # Evolve the rest of the population
+        while len(pop_prime) < len(pop):
+            # Select parents from population the representations of the parents
+            i1, i2 = pop.select(), pop.select()
+
+            # Apply crossover ~ probability is given to function
+            i1, i2 = pop.crossover(i1, i2)
+
+            for i in [i1, i2]:
+                # Apply mutation ~ probability is given to function
+                i = pop.mutation(i)
+                # Create individual and get its fitness
+                i = Individual(representation=i)
+                pop.fitness(i)
+
+            new_pop.append(i1)
+            if len(new_pop) < len(pop):
+                new_pop.append(i2)
+
+        pop.individuals = new_pop
+    return
+
